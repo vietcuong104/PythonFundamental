ab22df754c7836893bb3f714986893d1ed44aee1
phamvietcuong <vietcuong104@gmail.com>
2024-01-04 17:13:09 +0700

    update frequently

---
+++ BoxPlot/boxPlot.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/Advertising.csv")
+print(data.head())
+
+import plotly.express as px
+fig = px.box(data, y="Radio")
+fig.show()
---
+++ SendInstaMessages/sendInstaMessages.py
@@ -0,0 +1,5 @@
+from instabot import Bot
+bot = Bot()
+
+bot.login(username = "mike_kintaru", password = "xxxx")
+bot.send_message("Hey bro", [17334086203])
